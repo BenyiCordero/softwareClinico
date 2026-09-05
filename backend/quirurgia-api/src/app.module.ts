8203628e8686c1cc314afc5fc741c4443f995663
Axel Cervantes <axelpartida584@gmail.com>
2026-09-05 10:18:19 -0600

    feat: :construction_worker: (feat): add backend init

---
+++ backend/quirurgia-api/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { createObserveModule } from '@nestjs/observe';
+
+export const { ObserveModule, ObserveInstrument } = createObserveModule();
+
+@Module({
+  imports: [
+    
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
